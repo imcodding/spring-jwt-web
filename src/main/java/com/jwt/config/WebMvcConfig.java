22204412813d5d8cb3bee6b036a30bd517fbb17c
sooyun <55366664+imcodding@users.noreply.github.com>
2022-05-25 12:05:31 +0900

    인터셉터와 JWT 적용

---
+++ src/main/java/com/jwt/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.jwt.config;
+
+import com.jwt.provider.AuthInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final AuthInterceptor authInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns("/api/v1/items/**")
+                .excludePathPatterns("/api/v1/login/**");
+    }
+}
---
+++ src/main/java/com/jwt/provider/AuthInterceptor.java
@@ -0,0 +1,51 @@
+package com.jwt.provider;
+
+import com.jwt.provider.JwtAuthProvider;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class AuthInterceptor implements HandlerInterceptor {
+
+    private final JwtAuthProvider jwtAuthProvider;
+    private static final String AUTHORIZATION_HEADER = "x-auth-token";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("auth intercept!");
+
+        Optional<String> token = resolveToken(request);
+
+        if(token.isPresent()) {
+            jwtAuthProvider.setToken(token.get()); //클라이언트에게 받은 토큰
+            if(jwtAuthProvider.validate()) {
+                log.info("jwt valid");
+                return true;
+            } else {
+                log.info("jwt invalid");
+                return false;
+            }
+        } else {
+            log.info("jwt is not included in the header");
+            return false;
+        }
+    }
+
+    private Optional<String> resolveToken(HttpServletRequest request) {
+        String authToken = request.getHeader(AUTHORIZATION_HEADER);
+        if (StringUtils.hasText(authToken)) {
+            return Optional.of(authToken);
+        } else {
+            return Optional.empty();
+        }
+    }
+}
